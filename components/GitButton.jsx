8b62d673ec3d2ae46fca135cbca8fd737d6467cf
sharagulov <justcausegame@yandex.ru>
2024-09-25 21:20:54 +0300

    v2

--- components/GitButton.jsx
+++ components/GitButton.jsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 import { signIn } from 'next-auth/react'
 import { Button } from "@/components/ui/button";
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from "react";
 
 const GitButton = () => {
   const searchParams = useSearchParams()
   const callbackUrl = searchParams.get('callbackUrl') || '/profile';
 
   return (
+    <Suspense>
+
     <Button variant="white" className="w-full" onClick={() => signIn('github', { callbackUrl })}>
       <div className="flex items-center gap-2">
         <div className="w-[15px] h-[15px]">
@@ -17,12 +20,12 @@ const GitButton = () => {
             width={15}
             height={15}
             alt="github"
-          />
+            />
         </div>
         <span>GH</span>
       </div>
-
     </Button>
+</Suspense>
   )
 }
 
