c89a8cbcc6d9cac2c308e417127c61434fc952e9
sharagulov <justcausegame@yandex.ru>
2024-09-25 22:28:14 +0300

    vercelization

--- components/AuthBlock.jsx
+++ components/AuthBlock.jsx
@@ -1,19 +1,22 @@
 'use client'
 import YandexButton from "./YandexButton";
 import GitButton from './GitButton'
+import { Suspense } from "react";
 
 const AuthBlock = () => {
 
   return (
     <div className='flex justify-center'>
-        <div className="mt-2 w-full  text-center">
-          <span className="text-black/50 text-sm ">другие способы входа</span>
-          <div className="flex mt-2 gap-3 p-[20px] shadow-xl  rounded-xl">
+      <div className="mt-2 w-full  text-center">
+        <span className="text-black/50 text-sm ">другие способы входа</span>
+        <div className="flex mt-2 gap-3 p-[20px] shadow-xl  rounded-xl">
+          <Suspense>
             <GitButton />
             <YandexButton />
-          </div>
+          </Suspense>
         </div>
       </div>
+    </div>
   )
 }
 
