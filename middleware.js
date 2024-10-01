049cb1e753d7cce06fcf873d788a84cd2f49152c
sharagulov <justcausegame@yandex.ru>
2024-10-02 00:31:14 +0300

    CORS in middleware

--- middleware.js
+++ middleware.js
@@ -1,2 +1,27 @@
+import { NextResponse } from 'next/server';
+
 export { default } from "next-auth/middleware"
-export const config = {matcher:['/profile', '/protected/:path*']}
+export const config = {matcher:['/profile', '/protected/:path*', '/api/:path*']}
+export function middleware(request) {
+  const response = NextResponse.next();
+
+  // Настройте разрешенные источники
+  const allowedOrigins = ['https://safethrow-server.ru'];
+  const origin = request.headers.get('origin');
+
+  if (origin && allowedOrigins.includes(origin)) {
+    response.headers.set('Access-Control-Allow-Origin', "*");
+  }
+
+  // Настройте другие заголовки CORS
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+
+  // Обработка предварительных запросов OPTIONS
+  if (request.method === 'OPTIONS') {
+    return new NextResponse(null, { status: 200 });
+  }
+
+  return response;
+}
