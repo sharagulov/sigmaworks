c572707a8520f7351953f1efe848e909847e9442
sharagulov <justcausegame@yandex.ru>
2024-10-21 21:01:30 +0300

    blockchain doesnt inserts into vercel

--- blockchainCopy/test-network/skey.sh
+++
@@ -1,7 +0,0 @@
-#cloud-config
-echo "Начинаем выполнение команд..."
-./network.sh down
-./network.sh up createChannel -c userchannel -ca
-./network.sh up createChannel -c fileschannel -ca
-./network.sh deployCC -ccn userbasic -ccp ../asset-transfer-basic/user/chaincode-typescript/ -ccl typescript -c userchannel
-./network.sh deployCC -ccn filesbasic -ccp ../asset-transfer-basic/files/chaincode-typescript/ -ccl typescript -c fileschannel
